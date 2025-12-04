89ea5dcbcfd75e601d21d46db39b3a0efe196d16
datdat <datdat@hp.lan>
2025-12-05 02:27:34 +0700

    feat:add list and download by path

--- ServerSide/main.py
+++ ServerSide/main.py
@@ -34,16 +34,95 @@ import time
 from typing import Dict, Tuple
 
 HOST = "0.0.0.0"
-PORT = 9000
+PORT = 9001
 ENCODING = "utf-8"
 STORAGE_DIR = os.path.join(os.getcwd(), "storage")
+STORAGE_DIR = os.path.realpath(STORAGE_DIR)
 RECV_BUFFER = 8192
+
+SOUND_EXTENSIONS = {"mp3", "m4p", "m4a", "flac"}
+VIDEO_EXTENSIONS = {"mp4", "mkv", "webm", "flv"}
+TEXT_EXTENSIONS = {"txt", "doc", "docx"}
+IMAGE_EXTENSIONS = {"jpg", "jpeg", "png", "bmp"}
+COMPRESSED_EXTENSIONS = {"7z", "rar", "zip"}
 
 if not os.path.exists(STORAGE_DIR):
     os.makedirs(STORAGE_DIR, exist_ok=True)
 
+def _safe_path(requested_path):
+    safe_path = requested_path.lstrip("/")
+    full_path = os.path.join(STORAGE_DIR, safe_path)
+
+    real_path = os.path.realpath(full_path)
+
+    if os.path.commonpath([real_path, STORAGE_DIR]) == STORAGE_DIR: #commonpath for more safe than commonprefix
+        return real_path
+
+    raise ValueError("Invalid path")
+
 clients_lock = threading.Lock()
 clients = {}  # socket -> {"addr": (host,port), "name": optional}
+
+def is_end_with(file_type, path):
+    try:
+        extension = path.split(".")[-1].lower()
+    except IndexError:
+        return False  # No extension
+
+    if file_type == "sound":
+        return extension in SOUND_EXTENSIONS
+    if file_type == "video":
+        return extension in VIDEO_EXTENSIONS
+    if file_type == "text":
+        return extension in TEXT_EXTENSIONS
+    if file_type == "image":
+        return extension in IMAGE_EXTENSIONS
+    if file_type == "compressed":
+        return extension in COMPRESSED_EXTENSIONS
+    if file_type == "all":
+        return True
+
+    # Check for a specific custom extension (ex: "pdf")
+    return extension == file_type.lower()
+
+
+def load_directory(root_path, filters):
+
+    if not os.path.isdir(root_path):
+        return {"error": "Path is not a valid directory."}
+
+    dir_tree = {
+        "name": os.path.basename(root_path),
+        "path": root_path,
+        "subdirectories": [],
+        "files": [],
+    }
+
+    dir_map = {root_path: dir_tree}
+
+    for parent_path, subdirs, files in os.walk(root_path):
+        parent_node = dir_map[parent_path]
+
+        # add sub-dictionaries
+        if "folder" not in filters:
+            for subdir_name in subdirs:
+                sub_path = os.path.join(parent_path, subdir_name)
+                subdir_node = {
+                    "name": subdir_name,
+                    "path": sub_path,
+                    "subdirectories": [],
+                    "files": [],
+                }
+                parent_node["subdirectories"].append(subdir_node)
+                dir_map[sub_path] = subdir_node
+
+        #add files
+        for filename in files:
+            file_path = os.path.join(parent_path, filename)
+            if any(is_end_with(f, file_path) for f in filters):
+                parent_node["files"].append({"name": filename, "path": file_path})
+
+    return dir_tree
 
 
 def _recv_all(sock: socket.socket, n: int) -> bytes:
@@ -137,8 +216,11 @@ def handle_upload(sock: socket.socket, payload: dict):
         _send_control(sock, {"type": "error", "payload": "Invalid upload parameters"})
         return
 
-    safe_name = os.path.basename(name)
-    dst_path = os.path.join(STORAGE_DIR, safe_name)
+    try:
+        dst_path = _safe_path(name)
+    except ValueError:
+        _send_control(sock, {"type": "error", "payload": "Invalid path"})
+        return
 
     # If file exists, we overwrite (could be changed to reject)
     _send_control(sock, {"type": "ready", "payload": None})
@@ -178,6 +260,7 @@ def handle_upload(sock: socket.socket, payload: dict):
             sock,
             {"type": "upload_result", "payload": {"ok": True, "sha256": actual_sha}},
         )
+        safe_name = os.path.basename(dst_path)
         print(f"Uploaded file: {safe_name} ({size} bytes) sha256={actual_sha}")
         _broadcast_system(f"file_added:{safe_name}")
     except Exception as e:
@@ -191,19 +274,17 @@ def handle_upload(sock: socket.socket, payload: dict):
         )
 
 
-def handle_download(sock: socket.socket, payload: dict):
-    name = payload.get("name")
-    if not name:
-        _send_control(sock, {"type": "error", "payload": "Missing name for download"})
-        return
-    safe_name = os.path.basename(name)
-    path = os.path.join(STORAGE_DIR, safe_name)
+def handle_download(sock: socket.socket, path: str):
+    # The `path` is expected to be an absolute and safe path from `_safe_path()`
     if not os.path.exists(path) or not os.path.isfile(path):
         _send_control(sock, {"type": "error", "payload": "file_not_found"})
         return
+    
     size = os.path.getsize(path)
     sha = _file_sha256(path)
+
     _send_control(sock, {"type": "ready", "payload": {"size": size, "sha256": sha}})
+    
     # Stream file bytes
     try:
         with open(path, "rb") as f:
@@ -221,16 +302,23 @@ def handle_delete(sock: socket.socket, payload: dict):
     if not name:
         _send_control(sock, {"type": "error", "payload": "Missing name for delete"})
         return
-    safe_name = os.path.basename(name)
-    path = os.path.join(STORAGE_DIR, safe_name)
-    if not os.path.exists(path):
-        _send_control(sock, {"type": "error", "payload": "file_not_found"})
+    try:
+        path = _safe_path(name)
+    except ValueError:
+        _send_control(sock, {"type": "error", "payload": "Invalid path"})
         return
+    
+    if not os.path.exists(path):
+        _send_control(sock, {"type": "error", "payload": "Invalid path"})
+        return
+    
     try:
         os.remove(path)
         _send_control(sock, {"type": "delete_result", "payload": {"ok": True}})
+        safe_name = os.path.basename(path)
         _broadcast_system(f"file_removed:{safe_name}")
         print(f"Deleted file: {safe_name}")
+
     except Exception as e:
         _send_control(
             sock, {"type": "delete_result", "payload": {"ok": False, "reason": str(e)}}
@@ -243,27 +331,64 @@ def handle_client(client_sock: socket.socket, addr: Tuple[str, int]):
     print(f"New client: {addr}")
     try:
         while True:
-            ctrl = _recv_control(client_sock)
-            if ctrl is None:
+            data = client_sock.recv(4096)
+            if not data:
                 break
-            typ = ctrl.get("type")
-            payload = ctrl.get("payload")
-            if typ == "list":
-                files = _list_storage()
-                _send_control(client_sock, {"type": "list", "payload": files})
+            jsonData = data.decode(ENCODING)
 
-            elif typ == "upload":
+            print(f"--> Json requested: {jsonData}")
+
+            jsonData = json.loads(jsonData)
+
+            if jsonData is None:
+                print("error")
+                break
+
+            command = jsonData.get("command")
+            payload = jsonData.get("payload")
+            path = jsonData.get("path")
+            filters = jsonData.get("filters")
+
+            try:
+                if path:
+                    path = _safe_path(path)
+                else:
+                    path = STORAGE_DIR
+            except ValueError as e:
+                _send_control(client_sock, {"type": "error", "payload": str(e)})
+                break
+            
+            if payload is None:
+                payload = {}
+            
+            # if path is None:
+            #     path = STORAGE_DIR
+
+            if filters is None:
+                filters = ["all"]
+            
+            print(f"path requested: {path}")
+
+            if command == "list":
+                if not os.path.isdir(path):
+                    _send_control(client_sock, {"type": "error", "payload": "file_not_found"})
+                    continue
+
+                files = load_directory(path, filters)
+                _send_control(client_sock, {"type": "list", "payload": files})
+            
+            elif command == "upload":
                 # payload: {"name":..., "size":..., "sha256":...}
                 handle_upload(client_sock, payload or {})
 
-            elif typ == "download":
+            elif command == "download":
                 # payload: {"name":...}
-                handle_download(client_sock, payload or {})
+                handle_download(client_sock, path)
 
-            elif typ == "delete":
+            elif command == "delete":
                 handle_delete(client_sock, payload or {})
 
-            elif typ == "ping":
+            elif command == "ping":
                 _send_control(client_sock, {"type": "pong", "payload": None})
 
             else:
